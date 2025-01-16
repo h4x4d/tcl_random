d7748c24fd96c945b9028b9dc416c7fda145ccdf
h4x4d <sidenkoleg@gmail.com>
2025-01-16 15:36:21 +0300

    chances fix

--- bundle.py
+++ bundle.py
@@ -11,17 +11,17 @@ class Bundle:
 
     def open(self):
         rarities = [random.randint(1, 100) for _ in range(5)]
-        if all(i > 35 for i in rarities):
+        if all(i > 34 for i in rarities):
             rarities[0] = 20
         random.shuffle(rarities)
 
         cards = []
         for i in rarities:
-            if i < 4:
+            if i < 3:
                 cards.append(random.choice(self.leg))
-            elif i < 16:
+            elif i < 15:
                 cards.append(random.choice(self.epic))
-            elif i < 36:
+            elif i < 35:
                 cards.append(random.choice(self.rare))
             else:
                 cards.append(random.choice(self.common))
